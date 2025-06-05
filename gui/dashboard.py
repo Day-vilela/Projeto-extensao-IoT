b279f3f08b984b1ae2458d83a548a1456a30e87f
Day-vilela <dayamano64@gmail.com>
2025-06-05 16:38:28 -0300

    refactor: upgrade dashboard

--- gui/dashboard.py
+++ gui/dashboard.py
@@ -4,17 +4,23 @@ from tkinter import ttk
 class Dashboard:
     def __init__(self, root):
         self.root = root
-        self.root.title("Simulador de Ambiente Inteligente")
+        self.root.title("Ambiente Inteligente") #Atualizar: titulo
+
+        self.sensor_map = {
+            'Temperatura' : ('temp', '°C'),
+            'Umidade' : ('hum', '%'),
+            'Qualidade do Ar' : ('AQI', ''),
+            'Luz' : ('light', 'lux'),
+            'Ruído' : ('noise', 'dB')
+        }
 
         self.labels = {}
-        for sensor in ['Temperatura', 'Umidade', 'Qualidade do Ar', 'Luz', 'Ruído']:
-            lbl = ttk.Label(root, text=f"{sensor}: ---")
-            lbl.pack(padx=10, pady=5)
-            self.labels[sensor] = lbl
+        for i, (nome, (chave, unidade)) in enumerate(self.sensor_map.items()):
+            lbl = ttk.Label(root, text=f"{nome}: ---")
+            lbl.grid(row=i, column=0, sticky='w', padx=10, pady=5)
+            self.labels[nome] = lbl
 
     def update(self, data):
-        self.labels['Temperatura'].config(text=f"Temperatura: {data['temp']} °C")
-        self.labels['Umidade'].config(text=f"Umidade: {data['hum']} %")
-        self.labels['Qualidade do Ar'].config(text=f"AQI: {data['aqi']}")
-        self.labels['Luz'].config(text=f"Luz: {data['light']} lux")
-        self.labels['Ruído'].config(text=f"Ruído: {data['noise']} dB")
+        for nome, (chave, unidade) in self.sensor_map.items():
+            valor = data.get(chave, '---')
+            self.labels[nome].config(text=f"{nome}: {valor} {unidade}")
